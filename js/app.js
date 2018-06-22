e6656f131ae047925178a16a0ef2515c021b700b
CindyBischof <cindy.bischof@gmail.com>
2018-06-22 08:25:24 -0500

    Adds allCards & openCards variables, event listener for clicks & adds/removes .open and .show classes

--- js/app.js
+++ js/app.js
@@ -36,3 +36,28 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ //variable hold all cards
+ var allCards = document.querySelectorAll('.card');
+ //Array to hold cards that are open. Initially empty.
+ var openCards = [];
+
+ //event listner for clicks on cards
+ allCards.forEach(function(card) {
+   card.addEventListener('click', function(e) {
+     //when a card is clicked, the card gets added to the openCards array
+     openCards.push(card);
+     //adds .open and .show classes when card is clicked
+     card.classList.add('open', 'show');
+     console.log('Open Cards:', openCards.length);
+     //if 2 or more cards are showing, hide the cards again
+     if (openCards.length == 2) {
+       setTimeout(function() {
+         openCards.forEach(function(card) {
+           card.classList.remove('open', 'show');
+         });
+
+         openCards = [];
+       }, 1000);
+     }
+   });
+ });
